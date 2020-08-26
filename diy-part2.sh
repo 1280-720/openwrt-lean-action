afc8a27f99f845247be5e1c39d1b7aeab00e4c35
jjm2473 <jjm2473@gmail.com>
2020-08-26 10:20:40 +0800

    add passwall

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+echo 'src-git diy1 https://github.com/xiaorouji/openwrt-package.git;master' >> feeds.conf.default
+
+./scripts/feeds update diy1
+./scripts/feeds install luci-app-passwall
